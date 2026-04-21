d6907c16bf6471831edea8eeffb41b7fea2aa11d
adithya-naik <jatothadithyanaik@gmail.com>
2026-04-21 20:26:51 +0530

    refactor : replace with express server

--- Backend-Refresher/index.js
+++ Backend-Refresher/index.js
@@ -1,18 +1,31 @@
-import http from "http"
+// import http from "http"
 
-const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    res.end("Home page")
-  }
-  else if (req.url == "/about") {
-    res.end("About page")
-  }
-  else{
-    res.end("404 Error")
-  }
+// const server = http.createServer((req, res) => {
+//   if (req.url == "/") {
+//     res.end("Home page")
+//   }
+//   else if (req.url == "/about") {
+//     res.end("About page")
+//   }
+//   else{
+//     res.end("404 Error")
+//   }
+// })
+
+// server.listen(8000, () => {
+//   console.log("Server Started")
+// }
+// )
+
+import express from "express"
+
+const app = express()
+const PORT = 8000
+
+app.get("/",(req,res)=>{
+  res.send("Its Home Page")
 })
 
-server.listen(8000, () => {
-  console.log("Server Started")
-}
-)
+app.listen(PORT,()=>{
+  console.log(`Server Started at ${PORT}`)
+})

66ec272b9f243ae384d626364c9955c8e12970b0
adithya-naik <jatothadithyanaik@gmail.com>
2026-05-17 00:10:46 +0530

    add : post route

--- Backend-Refresher/client/src/App.jsx
+++ Backend-Refresher/client/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import axios from "axios";
+import { useState } from "react";
 const App = () => {
   const SERVER_URL = "http://localhost:8000/";
+  const [userData, setUserData] = useState({
+    name: "",
+    age: "",
+    city: "",
+  });
+
+  // Get Method
   async function handleClick() {
     try {
       // This gives CROS error, so add this origin in server
@@ -12,8 +20,24 @@ const App = () => {
 
       const result = await axios.get(`${SERVER_URL}`);
       // access the data from .data
-      console.log("Result from axios : ", result  );
+      console.log("Result from axios : ", result);
       console.log("Result from axios : ", result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Post Method
+  async function handleSubmit() {
+    try {
+      const result = await axios.post(`${SERVER_URL}`, {
+        name: userData.name,
+        age: userData.age,
+        city: userData.city,
+      });
+      // access the data from .data
+      console.log("From Server result: ", result);
+      console.log("From Server data : ", result.data);
     } catch (error) {
       console.log(error);
     }
@@ -21,7 +45,33 @@ const App = () => {
 
   return (
     <div>
+      <p>This gets data from "/" route : </p>
       <button onClick={() => handleClick()}>Send</button>
+
+      <div>
+        <p>This posts data to "/" route : </p>
+        <input
+          type="text"
+          placeholder="name"
+          value={userData.name}
+          onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+        />
+
+        <input
+          type="number"
+          placeholder="age"
+          value={userData.age}
+          onChange={(e) => setUserData({ ...userData, age: e.target.value })}
+        />
+
+        <input
+          type="text"
+          placeholder="city"
+          value={userData.city}
+          onChange={(e) => setUserData({ ...userData, city: e.target.value })}
+        />
+        <button onClick={() => handleSubmit()}>Submit</button>
+      </div>
     </div>
   );
 };
--- Backend-Refresher/server/index.js
+++ Backend-Refresher/server/index.js
@@ -2,13 +2,22 @@ import express from "express"
 import cors from "cors"
 const app = express()
 const PORT = 8000
-// check what happens if we dont add json in middleware
 app.use(cors({
   origin: "http://localhost:5173"
 }))
+// check what happens if we dont add json in middleware
+// Above question is answered
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello Server is Running !!" })
+})
+
+// This will give error if u dont mention express.json in middle ware : Without `express.json()`, Express cannot parse JSON request data, so `req.body` becomes `undefined`.
+
+app.post("/", (req, res) => {
+  console.log(req.body)
+  res.json({ succes: "true", recievedData: req.body })
 })
 
 app.listen(PORT, () => {

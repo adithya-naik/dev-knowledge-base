57493963907e7a644e9dfcec6b28bf3ee1441cdf
adithya-naik <jatothadithyanaik@gmail.com>
2026-05-16 22:51:20 +0530

    add : specific cors origin

--- Backend-Refresher/server/index.js
+++ Backend-Refresher/server/index.js
@@ -3,7 +3,9 @@ import cors from "cors"
 const app = express()
 const PORT = 8000
 // check what happens if we dont add json in middleware
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173"
+}))
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello Server is Running !!" })

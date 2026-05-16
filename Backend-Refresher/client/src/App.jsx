4d0a0f02e878ea76fdba577217eff6ac394c1c24
adithya-naik <jatothadithyanaik@gmail.com>
2026-05-16 22:48:31 +0530

    add : axios

--- Backend-Refresher/client/src/App.jsx
+++ Backend-Refresher/client/src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-
+import axios from "axios";
 const App = () => {
   const SERVER_URL = "http://localhost:8000/";
   async function handleClick() {
     try {
-      // learn how to use axios also
-      // when evr we use fecth convert the result into json
+      // This gives CROS error, so add this origin in server
+      // const result = await fetch(`${SERVER_URL}`);
+      // let data = await result.json();
+      // console.log("Result : ", result);
+      // console.log("Data : ", data);
 
-      // This gives CROS error, so add this origin in server  
-      const result = await fetch(`${SERVER_URL}`);
-      let data = await result.json();
-      console.log("Result : ", result);
-      console.log("Data : ", data);
-    } catch (error) {}
+      const result = await axios.get(`${SERVER_URL}`);
+      // access the data from .data
+      console.log("Result from axios : ", result  );
+      console.log("Result from axios : ", result.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
